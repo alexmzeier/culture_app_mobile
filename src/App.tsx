5d70b896c2e45f32cd2c275341730c150c553f8f
alexmzeier <alexander.zeier@outlook.de>
2020-12-21 15:44:37 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
+import Landing from './pages/Landing';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -31,6 +32,7 @@ const App: React.FC = () => (
         <Route path="/home" component={Home} exact={true} />
         <Route exact path="/" render={() => <Redirect to="/home" />} />
         <Route exact path="/signup" component={SignIn}/>
+        <Route exact path="/landing" component={Landing}/>
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
